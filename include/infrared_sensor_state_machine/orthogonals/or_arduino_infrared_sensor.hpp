3b01a69bbf4b40424f54ca15370adc5027625aa7
Scott <scott.duncan.work@gmail.com>
2022-07-17 19:36:06 +0100

    rename of folder and orthogonals now work.

---
+++ include/infrared_sensor_state_machine/orthogonals/or_arduino_infrared_sensor.hpp
@@ -0,0 +1,21 @@
+#ifndef OR_INFRARED_SENSOR_HPP
+#define OR_INFRARED_SENSOR_HPP
+
+#include <smacc/smacc_orthogonal.h>
+#include <infrared_sensor_state_machine/clients/cl_arduino_infrared_sensor/cl_arduino_infrared_sensor.hpp>
+
+namespace sm_infrared_sensor
+{
+    
+class OrArduinoInfraredSensor : public smacc::Orthogonal<OrArduinoInfraredSensor>
+{
+public:
+    virtual void onInitialize() override
+    {
+        auto  arduino_infrared_sensor_client= this->createClient<ClArduinoInfraredSensor>("/sensor_activate");
+        arduino_infrared_sensor_client->initialize();
+    }
+};
+} // namespace sm_atomic
+
+#endif
--- src/orthogonals/or_arduino_infrared_sensor.cpp
+++ src/orthogonals/or_arduino_infrared_sensor.cpp
@@ -1,6 +1,6 @@
-#include <include/infra_red_sensor_state_machine/orthogonols/or_arduino_infrared_seonsor.hpp>
+#include "infrared_sensor_state_machine/orthogonals/or_arduino_infrared_sensor.hpp"
 
-namespace sm_infra_red_sensor {
+namespace sm_infrared_sensor {
 void OrArduinoInfraredSensor::onInitialize() {
   auto  arduino_infrared_sensor_client= this->createClient<ClArduinoInfraredSensor>("/sensor_activate");
   rfid_client->initialize();
